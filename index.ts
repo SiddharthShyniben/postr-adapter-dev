191793ddfc2e8681b6ec22ac6fef9fad7ed4a226
Siddharth <siddharth.muscat@gmail.com>
2021-09-13 10:38:57 +0530

    Basic adapter

---
+++ index.ts
@@ -0,0 +1,23 @@
+let apiKey: string | null = null;
+
+export function init(config: {apiKey: string}) {
+	apiKey = config.apiKey;
+}
+
+export function publish(contents: string, parsedFrontMatter: any) {
+	fetch('https://dev.to/api/articles', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'api-key': apiKey
+		},
+		body: JSON.stringify({
+			article: {
+				title: parsedFrontMatter.title,
+				body_markdown: contents, 
+				pulished: true, 
+				description: 'Testing 123'
+			}
+		})
+	})
+}
